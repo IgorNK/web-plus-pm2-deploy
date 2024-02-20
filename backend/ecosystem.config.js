a927476ac22a1f1c093c2e4607efb11908559f91
IgorNK <mettbs@gmail.com>
2024-02-20 23:02:43 +0000

    Changes to backend deploy sccript

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
 	ref: DEPLOY_REF,
 	repo: 'https://github.com/IgorNK/web-plus-pm2-deploy',
 	path: DEPLOY_PATH,
-	'pre-deploy': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
+	'pre-deploy': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
 	'post-deploy': 'npm --prefix ./backend i && npm --prefix ./backend run build && pm2 reload ./backend/ecosystem.config.js',
     },
   },
