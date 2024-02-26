9c8fe5fb4620369fe1280bc0435d65a33c4dce7b
IgorNK <mettbs@gmail.com>
2024-02-26 03:43:30 +0000

    Deploy script: repository is fetched through ssh now

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
         user: DEPLOY_USER,
 	host: DEPLOY_HOST,
 	ref: DEPLOY_REF,
-	repo: 'https://github.com/IgorNK/web-plus-pm2-deploy',
+	repo: 'git@github.com:IgorNK/web-plus-pm2-deploy.git',
 	path: DEPLOY_PATH,
 	'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
 	'post-deploy': 'npm --prefix ./backend i && npm --prefix ./backend run build && pm2 reload ./backend/ecosystem.config.js',
