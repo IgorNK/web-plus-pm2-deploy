eddb9b675a3dec403723aa8dbbdcda0a519057dc
IgorNK <mettbs@gmail.com>
2024-02-26 04:04:47 +0000

    Changes to deploy script: fetch through ssh

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/IgorNK/web-plus-pm2-deploy',
+      repo: 'git@github.com:IgorNK/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
       'post-deploy': 'npm --prefix ./frontend i && npm --prefix ./frontend run build',
     },
